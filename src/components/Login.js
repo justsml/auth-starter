c644b1a99d3d513b190e9e81211e5119215aab55
Dan Levy <dan@danlevy.net>
2019-11-12 11:07:20 -0700

    login progress

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
+
+/*
+1. send POST https://lambda-fake-auth.herokuapp.com/api/login
+2. if successful, save token
+3. if not, show message
+*/
 
 class Login extends React.Component {
   state = {
     credentials: {
-      username: '',
-      password: ''
-    }
+      username: "",
+      password: ""
+    },
+    isLoggedIn: false
   };
 
   handleChange = e => {
@@ -20,11 +27,32 @@ class Login extends React.Component {
 
   login = e => {
     e.preventDefault();
+    axios
+      .post(
+        "https://lambda-fake-auth.herokuapp.com/api/login",
+        this.state.credentials
+      )
+      .then(response => {
+        console.log("response", response);
+        const { data } = response;
+
+        sessionStorage.setItem("token", data.payload);
+        this.setState({ ...this.state, isLoggedIn: true });
+      });
   };
+
+  componentDidMount() {
+    if (sessionStorage.getItem("token")) {
+      this.setState({ ...this.state, isLoggedIn: true });
+    } else {
+      this.setState({ ...this.state, isLoggedIn: false });
+    }
+  }
 
   render() {
     return (
       <div>
+        <h2>{this.state.isLoggedIn ? "LOGGED IN!" : "Please login"}</h2>
         <form onSubmit={this.login}>
           <input
             type="text"
